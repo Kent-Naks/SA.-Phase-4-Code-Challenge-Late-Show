2b69a54e3467e722eb39e7931bff237a180d1059
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-10-16 23:14:17 +0300

    Episodes), implement GET routes for episodes and episode details.

--- app.py
+++ app.py
@@ -8,3 +8,34 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
+
+@app.route('/episodes', methods=['GET'])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([{"id": ep.id, "date": ep.date, "number": ep.number} for ep in episodes])
+
+@app.route('/episodes/<int:id>', methods=['GET'])
+def get_episode(id):
+    episode = Episode.query.get(id)
+    if episode:
+        appearances = [
+            {
+                "id": ap.id,
+                "rating": ap.rating,
+                "episode_id": ap.episode_id,
+                "guest_id": ap.guest_id,
+                "guest": {
+                    "id": ap.guest.id,
+                    "name": ap.guest.name,
+                    "occupation": ap.guest.occupation
+                }
+            }
+            for ap in episode.appearances
+        ]
+        return jsonify({
+            "id": episode.id,
+            "date": episode.date,
+            "number": episode.number,
+            "appearances": appearances
+        })
+    return jsonify({"error": "Episode not found"}), 404
